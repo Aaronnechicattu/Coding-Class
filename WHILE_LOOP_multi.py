5fe519bc4cd528e2f3ee0bd90c74959fad68b0e1
Aaronnechicattu <149309699+Aaronnechicattu@users.noreply.github.com>
2023-10-29 19:50:09 +0530

    Add files via upload

---
+++ WHILE_LOOP_multi.py
@@ -0,0 +1,18 @@
+
+i=1
+print("\nmultiplication table of 24")
+while i<=10:
+    print(24,"*",i,"=",24*i)
+    i+=1
+i=1
+print("\nmultiplication table of 29")
+while i<=10:
+    print(29,"*",i,"=",29*i)
+    i+=1
+i=1
+print("\nmultiplication table of 50")
+while i<=10:
+    print(50,"*",i,"=",50*i)
+    i+=1
+
+    
